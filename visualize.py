8fc4385d6793de8e4e6d864c197fd90d42cbfd98
samuki <s.kiegeland@gmx.de>
2023-05-23 15:14:22 +0200

    Added visualization script

---
+++ visualize.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+import seaborn as sns
+import plotly.express as px
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import plotly.figure_factory as ff
+
+import config
+import json
+import os
+
+def plot_examples(data_path, idx_list):
+    """
+    Plots the first train and test pairs of a specified task,
+    using same color scheme as the ARC app
+    """
+    training_tasks = sorted(os.listdir(data_path))
+    for i in idx_list:
+        task_file = str(data_path / training_tasks[i])
+        with open(task_file, 'r') as f:
+            task = json.load(f)
+        cmap = colors.ListedColormap(
+            ['#000000', '#0074D9','#FF4136','#2ECC40','#FFDC00',
+            '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
+        norm = colors.Normalize(vmin=0, vmax=9)
+        _, ax = plt.subplots(1, 4, figsize=(15,15))
+        ax[0].imshow(task['train'][0]['input'], cmap=cmap, norm=norm)
+        width = np.shape(task['train'][0]['input'])[1]
+        height = np.shape(task['train'][0]['input'])[0]
+        ax[0].set_xticks(np.arange(0,width))
+        ax[0].set_yticks(np.arange(0,height))
+        ax[0].set_xticklabels([])
+        ax[0].set_yticklabels([])
+        ax[0].tick_params(length=0)
+        ax[0].grid(True)
+        ax[0].set_title('Train Input')
+        ax[1].imshow(task['train'][0]['output'], cmap=cmap, norm=norm)
+        width = np.shape(task['train'][0]['output'])[1]
+        height = np.shape(task['train'][0]['output'])[0]
+        ax[1].set_xticks(np.arange(0,width))
+        ax[1].set_yticks(np.arange(0,height))
+        ax[1].set_xticklabels([])
+        ax[1].set_yticklabels([])
+        ax[1].tick_params(length=0)
+        ax[1].grid(True)
+        ax[1].set_title('Train Output')
+        ax[2].imshow(task['test'][0]['input'], cmap=cmap, norm=norm)
+        width = np.shape(task['test'][0]['input'])[1]
+        height = np.shape(task['test'][0]['input'])[0]
+        ax[2].set_xticks(np.arange(0,width))
+        ax[2].set_yticks(np.arange(0,height))
+        ax[2].set_xticklabels([])
+        ax[2].set_yticklabels([])
+        ax[2].tick_params(length=0)
+        ax[2].grid(True)
+        ax[2].set_title('Test Input')
+        ax[3].imshow(task['test'][0]['output'], cmap=cmap, norm=norm)
+        width = np.shape(task['test'][0]['output'])[1]
+        height = np.shape(task['test'][0]['output'])[0]
+        ax[3].set_xticks(np.arange(0,width))
+        ax[3].set_yticks(np.arange(0,height))
+        ax[3].set_xticklabels([])
+        ax[3].set_yticklabels([])
+        ax[3].tick_params(length=0)
+        ax[3].grid(True)
+        ax[3].set_title('Test Output')
+        plt.tight_layout()
+        plt.show()
+        
+        
+def main():
+    idx_list = [1]
+    plot_examples(config.TRAIN_PATH, idx_list)
+
+if __name__ == '__main__':
+    main()
