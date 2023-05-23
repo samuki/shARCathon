2800a71d2111d9bef4ba7d061b419118f699e8da
samuki <s.kiegeland@gmx.de>
2023-05-23 19:51:00 +0200

    better visualization

--- visualize.py
+++ visualize.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # PATH to dataset to visualize
 DATA_PATH = config.TRAIN_PATH
 # Indices of images to visualize
-IDX_LIST = [1]
+IDX_LIST = [1, 2]
 
 def plot_examples(data_path, idx_list):
     """
@@ -26,47 +26,19 @@ def plot_examples(data_path, idx_list):
             ['#000000', '#0074D9','#FF4136','#2ECC40','#FFDC00',
             '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
         norm = colors.Normalize(vmin=0, vmax=9)
-        _, ax = plt.subplots(1, 4, figsize=(15,15))
-        ax[0].imshow(task['train'][0]['input'], cmap=cmap, norm=norm)
-        width = np.shape(task['train'][0]['input'])[1]
-        height = np.shape(task['train'][0]['input'])[0]
-        ax[0].set_xticks(np.arange(0,width))
-        ax[0].set_yticks(np.arange(0,height))
-        ax[0].set_xticklabels([])
-        ax[0].set_yticklabels([])
-        ax[0].tick_params(length=0)
-        ax[0].grid(True)
-        ax[0].set_title('Train Input')
-        ax[1].imshow(task['train'][0]['output'], cmap=cmap, norm=norm)
-        width = np.shape(task['train'][0]['output'])[1]
-        height = np.shape(task['train'][0]['output'])[0]
-        ax[1].set_xticks(np.arange(0,width))
-        ax[1].set_yticks(np.arange(0,height))
-        ax[1].set_xticklabels([])
-        ax[1].set_yticklabels([])
-        ax[1].tick_params(length=0)
-        ax[1].grid(True)
-        ax[1].set_title('Train Output')
-        ax[2].imshow(task['test'][0]['input'], cmap=cmap, norm=norm)
-        width = np.shape(task['test'][0]['input'])[1]
-        height = np.shape(task['test'][0]['input'])[0]
-        ax[2].set_xticks(np.arange(0,width))
-        ax[2].set_yticks(np.arange(0,height))
-        ax[2].set_xticklabels([])
-        ax[2].set_yticklabels([])
-        ax[2].tick_params(length=0)
-        ax[2].grid(True)
-        ax[2].set_title('Test Input')
-        ax[3].imshow(task['test'][0]['output'], cmap=cmap, norm=norm)
-        width = np.shape(task['test'][0]['output'])[1]
-        height = np.shape(task['test'][0]['output'])[0]
-        ax[3].set_xticks(np.arange(0,width))
-        ax[3].set_yticks(np.arange(0,height))
-        ax[3].set_xticklabels([])
-        ax[3].set_yticklabels([])
-        ax[3].tick_params(length=0)
-        ax[3].grid(True)
-        ax[3].set_title('Test Output')
+        fig, axs = plt.subplots(1, 4, figsize=(15,15))
+        axs[0].imshow(task['train'][0]['input'], cmap=cmap, norm=norm)
+        axs[0].axis('off')
+        axs[0].set_title('Train Input')
+        axs[1].imshow(task['train'][0]['output'], cmap=cmap, norm=norm)
+        axs[1].axis('off')
+        axs[1].set_title('Train Output')
+        axs[2].imshow(task['test'][0]['input'], cmap=cmap, norm=norm)
+        axs[2].axis('off')
+        axs[2].set_title('Test Input')
+        axs[3].imshow(task['test'][0]['output'], cmap=cmap, norm=norm)
+        axs[3].axis('off')
+        axs[3].set_title('Test Output')
         plt.tight_layout()
         plt.show()
             
