0031f677cd391e5c92dd4d48b1a6ff2bbde9bb9e
Pasquale Jordan <jordanpa@ethz.ch>
2023-07-05 12:09:33 +0200

    Added error message for wrong prompt kind

--- gpt2/__main__.py
+++ gpt2/__main__.py
@@ -1,9 +1,12 @@
 
 from .basic import main as basic_main
 from .finetuned import main as finetuned_main
+from .prompts import PROMPT_KINDS
 
 import sys
 
+WRONG_PROMPTKIND_MSG = "Unexpected argument! Please use one of:\n" + \
+    "\n".join(["python -m gpt2 basic " + kind for kind in PROMPT_KINDS])
 WRONG_ARGS_MSG = """Unexpected argument! Please use one of:
 python -m gpt2 finetuned
 python -m gpt2 basic"""
@@ -18,6 +21,9 @@ if __name__ == '__main__':
         exit(1)
     if (sys.argv[1] == "basic"):
         kind = sys.argv[2] if len(sys.argv) > 2 else 'basic'
+        if kind not in PROMPT_KINDS:
+            print(WRONG_PROMPTKIND_MSG)
+            exit(1)
         basic_main(kind=kind)
     elif (sys.argv[1] == "finetuned"):
         finetuned_main()
