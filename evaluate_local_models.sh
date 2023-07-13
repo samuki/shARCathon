d3c9a8258516301976adc222bf4a93e95d5ea9e8
Pasquale Jordan <jordanpa@ethz.ch>
2023-07-13 11:49:35 +0200

    Minor evaluation script change

--- evaluate_local_models.sh
+++ evaluate_local_models.sh
@@ -14,70 +14,70 @@ mv ./data/evaluation_small ./data/evaluation
 # This script tests all possible versions
 
 # # GPT 2
-# echo "Evaluating GPT2"
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic tiny
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic tiny
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step tiny
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step tiny
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns tiny
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns tiny
-# 
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic small
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic small
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step small
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step small
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns small
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns small
-# 
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic normal
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic normal
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step normal
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step normal
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns normal
-# TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns normal
+echo "Evaluating GPT2"
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic tiny
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic tiny
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step tiny
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step tiny
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns tiny
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns tiny
+
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic small
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic small
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step small
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step small
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns small
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns small
+
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic basic normal
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned basic normal
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic step-by-step normal
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned step-by-step normal
+TOKENIZERS_PARALLELISM="false" python -m gpt2 basic apply-patterns normal
+TOKENIZERS_PARALLELISM="false" python -m gpt2 finetuned apply-patterns normal
 
 # GPT Neo
-# echo "Evaluating GPT Neo"
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic tiny
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic tiny
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step tiny
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step tiny
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns tiny
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns tiny
-# 
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic small
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic small
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step small
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step small
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns small
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns small
-# 
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic normal
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic normal
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step normal
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step normal
-# TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns normal
-# # TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns normal
+echo "Evaluating GPT Neo"
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic tiny
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step tiny
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns tiny
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic tiny
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step tiny
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns tiny
+
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic small
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step small
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns small
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic small
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step small
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns small
+
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic basic normal
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic step-by-step normal
+TOKENIZERS_PARALLELISM="false" python -m gptneo basic apply-patterns normal
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned basic normal
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned step-by-step normal
+# TOKENIZERS_PARALLELISM="false" python -m gptneo finetuned apply-patterns normal
 
 # GPT J (20 Gig are not enough)
-echo "Evaluating GPT J"
-TOKENIZERS_PARALLELISM="false" python -m gptj basic basic tiny
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic tiny
-TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step tiny
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step tiny
-TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns tiny
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns tiny
-
-TOKENIZERS_PARALLELISM="false" python -m gptj basic basic small
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic small
-TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step small
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step small
-TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns small
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns small
-
-TOKENIZERS_PARALLELISM="false" python -m gptj basic basic normal
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic normal
-TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step normal
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step normal
-TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns normal
-# TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns normal
+# echo "Evaluating GPT J"
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic basic tiny
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic tiny
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step tiny
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step tiny
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns tiny
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns tiny
+# 
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic basic small
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic small
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step small
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step small
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns small
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns small
+# 
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic basic normal
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned basic normal
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic step-by-step normal
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned step-by-step normal
+# TOKENIZERS_PARALLELISM="false" python -m gptj basic apply-patterns normal
+# # TOKENIZERS_PARALLELISM="false" python -m gptj finetuned apply-patterns normal
